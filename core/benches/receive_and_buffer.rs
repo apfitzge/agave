a09f43c47bd37c3ad6b571006fad0f1e4a08ec7c
Andrew Fitzgerald <apfitzge@gmail.com>
2025-04-08 09:40:55 -0500

    bench

--- core/benches/receive_and_buffer.rs
+++ core/benches/receive_and_buffer.rs
@@ -1,15 +1,17 @@
 use {
     agave_banking_stage_ingress_types::BankingPacketBatch,
+    bytes::{Bytes, BytesMut},
     criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput},
     crossbeam_channel::{unbounded, Receiver},
+    itertools::Itertools,
     rand::prelude::*,
     solana_core::banking_stage::{
         decision_maker::BufferedPacketsDecision,
         packet_deserializer::PacketDeserializer,
         transaction_scheduler::{
             receive_and_buffer::{
-                ReceiveAndBuffer, SanitizedTransactionReceiveAndBuffer,
-                TransactionViewReceiveAndBuffer,
+                NoCopyTransactionViewReceiveAndBuffer, PacketBatchBytes, ReceiveAndBuffer,
+                SanitizedTransactionReceiveAndBuffer, TransactionViewReceiveAndBuffer,
             },
             scheduler_metrics::{SchedulerCountMetrics, SchedulerTimingMetrics},
             transaction_state_container::StateContainer,
@@ -28,6 +30,7 @@ use {
         hash::Hash,
         instruction::{AccountMeta, Instruction},
         message::{Message, VersionedMessage},
+        packet::PACKET_DATA_SIZE,
         signer::Signer,
         transaction::VersionedTransaction,
     },
@@ -135,6 +138,79 @@ fn generate_transactions(
     BankingPacketBatch::new(to_packet_batches(&txs, NUM_PACKETS))
 }
 
+fn generate_no_copy_transactions(
+    num_txs: usize,
+    bank: Arc<Bank>,
+    fee_payers: &[Keypair],
+    num_instructions_per_tx: usize,
+    probability_invalid_blockhash: f64,
+    set_rand_cu_price: bool,
+) -> Arc<PacketBatchBytes> {
+    assert!(num_instructions_per_tx <= MAX_INSTRUCTIONS_PER_TRANSACTION);
+    if set_rand_cu_price {
+        assert!(num_instructions_per_tx > 0,
+            "`num_instructions_per_tx` must be at least 1 when `set_rand_cu_price` flag is set to count\
+             the set_compute_unit_price instruction.");
+    }
+    let blockhash = FaultyBlockhash::new(bank.last_blockhash(), probability_invalid_blockhash);
+
+    let mut rng = rand::thread_rng();
+
+    let mut fee_payers = fee_payers.iter().cycle();
+
+    let mut bytes = BytesMut::with_capacity(num_txs * PACKET_DATA_SIZE);
+    let txs: Vec<Bytes> = (0..num_txs)
+        .map(|_| {
+            let fee_payer = fee_payers.next().unwrap();
+            let program_id = Pubkey::new_unique();
+
+            let mut instructions = Vec::with_capacity(num_instructions_per_tx);
+            if set_rand_cu_price {
+                // Experiments with different distributions didn't show much of the effect on the performance.
+                let compute_unit_price = rng.gen_range(0..1000);
+                instructions.push(ComputeBudgetInstruction::set_compute_unit_price(
+                    compute_unit_price,
+                ));
+            }
+            for _ in 0..num_instructions_per_tx.saturating_sub(1) {
+                instructions.push(Instruction::new_with_bytes(
+                    program_id,
+                    &[0],
+                    vec![AccountMeta {
+                        pubkey: fee_payer.pubkey(),
+                        is_signer: true,
+                        is_writable: true,
+                    }],
+                ));
+            }
+            let versioned_transaction = VersionedTransaction::try_new(
+                VersionedMessage::Legacy(Message::new_with_blockhash(
+                    &instructions,
+                    Some(&fee_payer.pubkey()),
+                    &blockhash.get(&mut rng),
+                )),
+                &[&fee_payer],
+            )
+            .unwrap();
+
+            let serialized_tx = bincode::serialize(&versioned_transaction).unwrap();
+            let serialized_tx_len = serialized_tx.len();
+            bytes.extend_from_slice(&serialized_tx);
+            let tx_bytes = bytes.split_to(serialized_tx_len);
+            tx_bytes.freeze()
+        })
+        .collect();
+
+    Arc::new(PacketBatchBytes {
+        batches: txs
+            .into_iter()
+            .chunks(NUM_PACKETS)
+            .into_iter()
+            .map(|chunk| chunk.collect())
+            .collect(),
+    })
+}
+
 trait ReceiveAndBufferCreator {
     fn create(
         receiver: Receiver<Arc<Vec<PacketBatch>>>,
@@ -160,6 +236,25 @@ impl ReceiveAndBufferCreator for SanitizedTransactionReceiveAndBuffer {
         bank_forks: Arc<RwLock<BankForks>>,
     ) -> Self {
         SanitizedTransactionReceiveAndBuffer::new(PacketDeserializer::new(receiver), bank_forks)
+    }
+}
+
+trait NoCopyReceiveAndBufferCreator {
+    fn create(
+        receiver: Receiver<Arc<PacketBatchBytes>>,
+        bank_forks: Arc<RwLock<BankForks>>,
+    ) -> Self;
+}
+
+impl NoCopyReceiveAndBufferCreator for NoCopyTransactionViewReceiveAndBuffer {
+    fn create(
+        receiver: Receiver<Arc<PacketBatchBytes>>,
+        bank_forks: Arc<RwLock<BankForks>>,
+    ) -> Self {
+        NoCopyTransactionViewReceiveAndBuffer {
+            receiver,
+            bank_forks,
+        }
     }
 }
 
@@ -240,6 +335,83 @@ fn bench_receive_and_buffer<T: ReceiveAndBuffer + ReceiveAndBufferCreator>(
     group.finish();
 }
 
+fn bench_no_copy_receive_and_buffer<T: ReceiveAndBuffer + NoCopyReceiveAndBufferCreator>(
+    c: &mut Criterion,
+    bench_name: &str,
+    num_instructions_per_tx: usize,
+    probability_invalid_blockhash: f64,
+    set_rand_cu_price: bool,
+) {
+    let GenesisConfigInfo {
+        mut genesis_config, ..
+    } = create_genesis_config(100_000);
+    let num_txs = 16 * 1024;
+    // This doesn't change the time to execute
+    let num_fee_payers = 1;
+    // fee payers will be verified, so have to create them properly
+    let fee_payers = create_accounts(num_fee_payers, &mut genesis_config);
+
+    let (bank, bank_forks) =
+        Bank::new_for_benches(&genesis_config).wrap_with_bank_forks_for_tests();
+    let bank_start = BankStart {
+        working_bank: bank.clone(),
+        bank_creation_time: Arc::new(Instant::now()),
+    };
+
+    let (sender, receiver) = unbounded();
+
+    let mut rb = T::create(receiver, bank_forks);
+
+    const TOTAL_BUFFERED_PACKETS: usize = 100_000;
+    let mut count_metrics = SchedulerCountMetrics::default();
+    let mut timing_metrics = SchedulerTimingMetrics::default();
+    let decision = BufferedPacketsDecision::Consume(bank_start);
+
+    let txs = generate_no_copy_transactions(
+        num_txs,
+        bank.clone(),
+        &fee_payers,
+        num_instructions_per_tx,
+        probability_invalid_blockhash,
+        set_rand_cu_price,
+    );
+    let mut container = <T as ReceiveAndBuffer>::Container::with_capacity(TOTAL_BUFFERED_PACKETS);
+
+    let mut group = c.benchmark_group("receive_and_buffer");
+    group.throughput(Throughput::Elements(num_txs as u64));
+    group.bench_function(bench_name, |bencher| {
+        bencher.iter_custom(|iters| {
+            let mut total: Duration = std::time::Duration::ZERO;
+            for _ in 0..iters {
+                // Setup
+                {
+                    if sender.send(txs.clone()).is_err() {
+                        panic!("Unexpectedly dropped receiver!");
+                    }
+
+                    // make sure container is empty.
+                    container.clear();
+                }
+
+                let start = Instant::now();
+                {
+                    let res = rb.receive_and_buffer_packets(
+                        &mut container,
+                        &mut timing_metrics,
+                        &mut count_metrics,
+                        &decision,
+                    );
+                    assert!(res.unwrap() == num_txs && !container.is_empty());
+                    black_box(&container);
+                }
+                total = total.saturating_add(start.elapsed());
+            }
+            total
+        })
+    });
+    group.finish();
+}
+
 fn bench_sanitized_transaction_receive_and_buffer(c: &mut Criterion) {
     bench_receive_and_buffer::<SanitizedTransactionReceiveAndBuffer>(
         c,
@@ -274,9 +446,27 @@ fn bench_transaction_view_receive_and_buffer(c: &mut Criterion) {
     );
 }
 
+fn bench_no_copy_view(c: &mut Criterion) {
+    bench_no_copy_receive_and_buffer::<NoCopyTransactionViewReceiveAndBuffer>(
+        c,
+        "no_copy_view_max_instructions",
+        MAX_INSTRUCTIONS_PER_TRANSACTION,
+        0.0,
+        true,
+    );
+    bench_no_copy_receive_and_buffer::<NoCopyTransactionViewReceiveAndBuffer>(
+        c,
+        "no_copy_view_min_instructions",
+        1,
+        0.0,
+        true,
+    );
+}
+
 criterion_group!(
     benches,
     bench_sanitized_transaction_receive_and_buffer,
-    bench_transaction_view_receive_and_buffer
+    bench_transaction_view_receive_and_buffer,
+    bench_no_copy_view
 );
 criterion_main!(benches);
