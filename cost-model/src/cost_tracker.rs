091393cce4dc8a26d5cfcb92f38cba4678f39ce2
Andrew Fitzgerald <apfitzge@gmail.com>
2025-08-15 12:56:22 -0500

    CostTracker::shared_block_cost

--- cost-model/src/cost_tracker.rs
+++ cost-model/src/cost_tracker.rs
@@ -9,7 +9,14 @@ use {
     solana_pubkey::Pubkey,
     solana_runtime_transaction::transaction_with_meta::TransactionWithMeta,
     solana_transaction_error::TransactionError,
-    std::{cmp::Ordering, collections::HashMap, num::Saturating},
+    std::{
+        collections::HashMap,
+        num::Saturating,
+        sync::{
+            atomic::{AtomicU64, Ordering},
+            Arc,
+        },
+    },
 };
 
 const WRITABLE_ACCOUNTS_PER_BLOCK: usize = 4096;
@@ -64,7 +71,7 @@ pub struct CostTracker {
     block_cost_limit: u64,
     vote_cost_limit: u64,
     cost_by_writable_accounts: HashMap<Pubkey, u64, ahash::RandomState>,
-    block_cost: u64,
+    block_cost: Arc<AtomicU64>,
     vote_cost: u64,
     transaction_count: Saturating<u64>,
     allocated_accounts_data_size: Saturating<u64>,
@@ -94,7 +101,7 @@ impl Default for CostTracker {
                 WRITABLE_ACCOUNTS_PER_BLOCK,
                 ahash::RandomState::new(),
             ),
-            block_cost: 0,
+            block_cost: Arc::new(AtomicU64::new(0)),
             vote_cost: 0,
             transaction_count: Saturating(0),
             allocated_accounts_data_size: Saturating(0),
@@ -120,7 +127,7 @@ impl CostTracker {
 
     pub fn reset(&mut self) {
         self.cost_by_writable_accounts.clear();
-        self.block_cost = 0;
+        self.block_cost.store(0, Ordering::Relaxed);
         self.vote_cost = 0;
         self.transaction_count = Saturating(0);
         self.allocated_accounts_data_size = Saturating(0);
@@ -177,7 +184,7 @@ impl CostTracker {
         self.would_fit(tx_cost)?;
         let updated_costliest_account_cost = self.add_transaction_cost(tx_cost);
         Ok(UpdatedCosts {
-            updated_block_cost: self.block_cost,
+            updated_block_cost: self.block_cost(),
             updated_costliest_account_cost,
         })
     }
@@ -194,14 +201,14 @@ impl CostTracker {
             .programs_execution_cost()
             .saturating_add(estimated_tx_cost.loaded_accounts_data_size_cost());
         match actual_load_and_execution_units.cmp(&estimated_load_and_execution_units) {
-            Ordering::Equal => (),
-            Ordering::Greater => {
+            std::cmp::Ordering::Equal => (),
+            std::cmp::Ordering::Greater => {
                 self.add_transaction_execution_cost(
                     estimated_tx_cost,
                     actual_load_and_execution_units - estimated_load_and_execution_units,
                 );
             }
-            Ordering::Less => {
+            std::cmp::Ordering::Less => {
                 self.sub_transaction_execution_cost(
                     estimated_tx_cost,
                     estimated_load_and_execution_units - actual_load_and_execution_units,
@@ -215,7 +222,12 @@ impl CostTracker {
     }
 
     pub fn block_cost(&self) -> u64 {
-        self.block_cost
+        self.block_cost.load(Ordering::Relaxed)
+    }
+
+    // TODO: return something that is readonly.
+    pub fn shared_block_cost(&self) -> Arc<AtomicU64> {
+        Arc::clone(&self.block_cost)
     }
 
     pub fn vote_cost(&self) -> u64 {
@@ -244,7 +256,7 @@ impl CostTracker {
             "cost_tracker_stats",
             "is_leader" => is_leader.to_string(),
             ("bank_slot", bank_slot, i64),
-            ("block_cost", self.block_cost, i64),
+            ("block_cost", self.block_cost(), i64),
             ("vote_cost", self.vote_cost, i64),
             ("transaction_count", self.transaction_count.0, i64),
             ("number_of_accounts", self.number_of_accounts(), i64),
@@ -306,7 +318,7 @@ impl CostTracker {
             }
         }
 
-        if self.block_cost.saturating_add(cost) > self.block_cost_limit {
+        if self.block_cost().saturating_add(cost) > self.block_cost_limit {
             // check against the total package cost
             return Err(CostTrackerError::WouldExceedBlockMaxLimit);
         }
@@ -382,7 +394,7 @@ impl CostTracker {
             *account_cost = account_cost.saturating_add(adjustment);
             costliest_account_cost = costliest_account_cost.max(*account_cost);
         }
-        self.block_cost = self.block_cost.saturating_add(adjustment);
+        self.block_cost.fetch_add(adjustment, Ordering::Relaxed);
         if tx_cost.is_simple_vote() {
             self.vote_cost = self.vote_cost.saturating_add(adjustment);
         }
@@ -403,7 +415,7 @@ impl CostTracker {
                 .or_insert(0);
             *account_cost = account_cost.saturating_sub(adjustment);
         }
-        self.block_cost = self.block_cost.saturating_sub(adjustment);
+        self.block_cost.fetch_sub(adjustment, Ordering::Relaxed);
         if tx_cost.is_simple_vote() {
             self.vote_cost = self.vote_cost.saturating_sub(adjustment);
         }
@@ -488,7 +500,7 @@ mod tests {
         assert_eq!(11, testee.block_cost_limit);
         assert_eq!(8, testee.vote_cost_limit);
         assert_eq!(0, testee.cost_by_writable_accounts.len());
-        assert_eq!(0, testee.block_cost);
+        assert_eq!(0, testee.block_cost());
     }
 
     #[test]
@@ -502,7 +514,7 @@ mod tests {
         let mut testee = CostTracker::new(cost, cost, cost);
         assert!(testee.would_fit(&tx_cost).is_ok());
         testee.add_transaction_cost(&tx_cost);
-        assert_eq!(cost, testee.block_cost);
+        assert_eq!(cost, testee.block_cost());
         assert_eq!(0, testee.vote_cost);
         let (_costliest_account, costliest_account_cost) = testee.find_costliest_account();
         assert_eq!(cost, costliest_account_cost);
@@ -519,7 +531,7 @@ mod tests {
         let mut testee = CostTracker::new(cost, cost, cost);
         assert!(testee.would_fit(&tx_cost).is_ok());
         testee.add_transaction_cost(&tx_cost);
-        assert_eq!(cost, testee.block_cost);
+        assert_eq!(cost, testee.block_cost());
         assert_eq!(cost, testee.vote_cost);
         let (_costliest_account, costliest_account_cost) = testee.find_costliest_account();
         assert_eq!(cost, costliest_account_cost);
@@ -566,7 +578,7 @@ mod tests {
             assert!(testee.would_fit(&tx_cost2).is_ok());
             testee.add_transaction_cost(&tx_cost2);
         }
-        assert_eq!(cost1 + cost2, testee.block_cost);
+        assert_eq!(cost1 + cost2, testee.block_cost());
         assert_eq!(1, testee.cost_by_writable_accounts.len());
         let (_ccostliest_account, costliest_account_cost) = testee.find_costliest_account();
         assert_eq!(cost1 + cost2, costliest_account_cost);
@@ -595,7 +607,7 @@ mod tests {
             assert!(testee.would_fit(&tx_cost2).is_ok());
             testee.add_transaction_cost(&tx_cost2);
         }
-        assert_eq!(cost1 + cost2, testee.block_cost);
+        assert_eq!(cost1 + cost2, testee.block_cost());
         assert_eq!(2, testee.cost_by_writable_accounts.len());
         let (_ccostliest_account, costliest_account_cost) = testee.find_costliest_account();
         assert_eq!(std::cmp::max(cost1, cost2), costliest_account_cost);
@@ -732,15 +744,15 @@ mod tests {
 
         assert!(testee.try_add(&tx_cost1).is_ok());
         assert!(testee.try_add(&tx_cost2).is_ok());
-        assert_eq!(cost1 + cost2, testee.block_cost);
+        assert_eq!(cost1 + cost2, testee.block_cost());
 
         // removing a tx_cost affects block_cost
         testee.remove(&tx_cost1);
-        assert_eq!(cost2, testee.block_cost);
+        assert_eq!(cost2, testee.block_cost());
 
         // add back tx1
         assert!(testee.try_add(&tx_cost1).is_ok());
-        assert_eq!(cost1 + cost2, testee.block_cost);
+        assert_eq!(cost1 + cost2, testee.block_cost());
 
         // cannot add tx1 again, cost limit would be exceeded
         assert!(testee.try_add(&tx_cost1).is_err());
@@ -767,7 +779,7 @@ mod tests {
             let tx_cost = simple_transaction_cost(&transaction, cost);
             assert!(testee.try_add(&tx_cost).is_ok());
             let (_costliest_account, costliest_account_cost) = testee.find_costliest_account();
-            assert_eq!(cost, testee.block_cost);
+            assert_eq!(cost, testee.block_cost());
             assert_eq!(3, testee.cost_by_writable_accounts.len());
             assert_eq!(cost, costliest_account_cost);
         }
@@ -782,7 +794,7 @@ mod tests {
             let tx_cost = simple_transaction_cost(&transaction, cost);
             assert!(testee.try_add(&tx_cost).is_ok());
             let (costliest_account, costliest_account_cost) = testee.find_costliest_account();
-            assert_eq!(cost * 2, testee.block_cost);
+            assert_eq!(cost * 2, testee.block_cost());
             assert_eq!(3, testee.cost_by_writable_accounts.len());
             assert_eq!(cost * 2, costliest_account_cost);
             assert_eq!(acct2, costliest_account);
@@ -799,7 +811,7 @@ mod tests {
             let tx_cost = simple_transaction_cost(&transaction, cost);
             assert!(testee.try_add(&tx_cost).is_err());
             let (costliest_account, costliest_account_cost) = testee.find_costliest_account();
-            assert_eq!(cost * 2, testee.block_cost);
+            assert_eq!(cost * 2, testee.block_cost());
             assert_eq!(3, testee.cost_by_writable_accounts.len());
             assert_eq!(cost * 2, costliest_account_cost);
             assert_eq!(acct2, costliest_account);
@@ -937,7 +949,7 @@ mod tests {
                     actual_loaded_accounts_data_size_cost,
                 );
 
-                assert_eq!(expected_cost, cost_tracker.block_cost);
+                assert_eq!(expected_cost, cost_tracker.block_cost());
                 assert_eq!(0, cost_tracker.vote_cost);
                 assert_eq!(
                     number_writeble_accounts,
@@ -971,7 +983,7 @@ mod tests {
         // assert cost_tracker is reverted to default
         assert_eq!(1, cost_tracker.transaction_count.0);
         assert_eq!(1, cost_tracker.number_of_accounts());
-        assert_eq!(cost, cost_tracker.block_cost);
+        assert_eq!(cost, cost_tracker.block_cost());
         assert_eq!(0, cost_tracker.vote_cost);
         assert_eq!(0, cost_tracker.allocated_accounts_data_size.0);
 
@@ -979,7 +991,7 @@ mod tests {
         // assert cost_tracker is reverted to default
         assert_eq!(0, cost_tracker.transaction_count.0);
         assert_eq!(0, cost_tracker.number_of_accounts());
-        assert_eq!(0, cost_tracker.block_cost);
+        assert_eq!(0, cost_tracker.block_cost());
         assert_eq!(0, cost_tracker.vote_cost);
         assert_eq!(0, cost_tracker.allocated_accounts_data_size.0);
     }
