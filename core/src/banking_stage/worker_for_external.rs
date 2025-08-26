f29c794ffad962cfa75dc29679da8e354f6441d7
Andrew Fitzgerald <apfitzge@gmail.com>
2025-08-26 16:15:31 -0500

    worker_for_external: execute

--- core/src/banking_stage/worker_for_external.rs
+++ core/src/banking_stage/worker_for_external.rs
@@ -1,8 +1,8 @@
 use {
-    crate::banking_stage::consumer::Consumer,
+    crate::banking_stage::{committer::CommitTransactionDetails, consumer::Consumer},
     agave_scheduler_bindings::{
         pack_message_flags, worker_message_types, PackToWorkerMessage, SharableTransaction,
-        WorkerToPackMessage,
+        WorkerToPackMessage, MAX_TRANSACTIONS_PER_PACK_MESSAGE,
     },
     agave_transaction_view::{
         resolved_transaction_view::ResolvedTransactionView, transaction_data::TransactionData,
@@ -11,8 +11,9 @@ use {
     rts_alloc::Allocator,
     solana_poh::poh_recorder::SharedWorkingBank,
     solana_pubkey::Pubkey,
-    solana_runtime::bank_forks::SharableBanks,
+    solana_runtime::{bank::Bank, bank_forks::SharableBanks},
     solana_runtime_transaction::runtime_transaction::RuntimeTransaction,
+    solana_transaction::sanitized::MessageHash,
     std::{
         ptr::NonNull,
         sync::{
@@ -55,22 +56,27 @@ pub struct WorkerForExternal {
     sharable_banks: SharableBanks,
     shared_working_bank: SharedWorkingBank,
     consumer: Consumer,
-
-    current_tx_indexes: Vec<usize>,
-    current_txs: Vec<RuntimeTransactionView>,
 }
 
 #[allow(dead_code)]
 impl WorkerForExternal {
     pub fn run(&mut self) {
+        // Pre-allocate scratch space for processing messages.
+        let mut current_tx_indexes = Vec::with_capacity(MAX_TRANSACTIONS_PER_PACK_MESSAGE);
+        let mut current_txs = Vec::with_capacity(MAX_TRANSACTIONS_PER_PACK_MESSAGE);
+
         while !self.exit.load(Ordering::Relaxed) {
             self.pack_message_consumer.sync();
-            self.process_loop();
+            self.process_loop(&mut current_tx_indexes, &mut current_txs);
             self.pack_message_consumer.finalize();
         }
     }
 
-    fn process_loop(&mut self) {
+    fn process_loop(
+        &mut self,
+        current_tx_indexes: &mut Vec<usize>,
+        current_txs: &mut Vec<RuntimeTransactionView>,
+    ) {
         if self.pack_message_consumer.is_empty() {
             // no work - sleep for a short duration.
             const SLEEP_DURATION: Duration = Duration::from_micros(100);
@@ -94,7 +100,10 @@ impl WorkerForExternal {
 
             // Depending on flags we may execute or proess the transaction differently.
             if message.flags & pack_message_flags::RESOLVE == 0 {
-                self.execute_message(message);
+                self.execute_message(message, current_tx_indexes, current_txs);
+
+                current_tx_indexes.clear();
+                current_txs.clear();
             } else {
                 self.resolve_message(message);
             }
@@ -132,10 +141,117 @@ impl WorkerForExternal {
         true
     }
 
-    fn execute_message(&mut self, message: &PackToWorkerMessage) {
+    fn execute_message(
+        &mut self,
+        message: &PackToWorkerMessage,
+        current_tx_indexes: &mut Vec<usize>,
+        current_txs: &mut Vec<RuntimeTransactionView>,
+    ) {
         debug_assert_eq!(message.flags & pack_message_flags::RESOLVE, 0);
 
-        todo!()
+        // Get the current working bank.
+        let Some(bank) = self.shared_working_bank.load() else {
+            for transaction_index in 0..usize::from(message.num_transactions) {
+                let sharable_transaction = &message.transactions[transaction_index];
+                self.not_included_transaction(sharable_transaction);
+            }
+            return;
+        };
+        if message.slot != bank.slot() {
+            for transaction_index in 0..usize::from(message.num_transactions) {
+                let sharable_transaction = &message.transactions[transaction_index];
+                self.not_included_transaction(sharable_transaction);
+            }
+            return;
+        }
+
+        for transaction_index in 0..usize::from(message.num_transactions) {
+            let tx_ptr =
+                self.tx_ptr_from_sharable_transaction(&message.transactions[transaction_index]);
+            if let Some(resolved_view) = Self::tx_ptr_to_resolved_transaction_view(tx_ptr, &bank) {
+                current_tx_indexes.push(transaction_index);
+                current_txs.push(resolved_view);
+            }
+        }
+
+        let output = self
+            .consumer
+            .process_and_record_transactions(&bank, current_txs.as_slice());
+
+        if let Ok(results) = output
+            .execute_and_commit_transactions_output
+            .commit_transactions_result
+        {
+            let mut attempted_transaction_index = 0;
+            let mut next_attempted_transaction_index =
+                current_tx_indexes.iter().copied().peekable();
+            for message_transaction_index in 0..usize::from(message.num_transactions) {
+                let sharable_transaction = &message.transactions[message_transaction_index];
+                if Some(&message_transaction_index) == next_attempted_transaction_index.peek() {
+                    let result = &results[attempted_transaction_index];
+
+                    match result {
+                        CommitTransactionDetails::Committed {
+                            compute_units,
+                            loaded_accounts_data_size: _,
+                            fee_payer_post_balance,
+                            result: _,
+                        } => {
+                            self.included_transaction(
+                                sharable_transaction,
+                                compute_units,
+                                fee_payer_post_balance,
+                            );
+                        }
+                        CommitTransactionDetails::NotCommitted(_err) => {
+                            self.not_included_transaction(sharable_transaction);
+                        }
+                    }
+
+                    attempted_transaction_index += 1;
+                    next_attempted_transaction_index.next();
+                } else {
+                    self.not_included_transaction(sharable_transaction);
+                }
+            }
+        }
+    }
+
+    fn included_transaction(
+        &mut self,
+        tx: &SharableTransaction,
+        compute_units: &u64,
+        fee_payer_balance: &u64,
+    ) {
+        let mut response = self.reserve_response();
+        // SAFETY: `response` is a valid pointer to a `WorkerToPackMessage`.
+        let response = unsafe { response.as_mut() };
+
+        response.tag = worker_message_types::INCLUDED;
+        // SAFETY: `response` is a valid pointer to a `WorkerToPackMessage` and we've just set the tag.
+        let included = unsafe { &mut response.inner.included };
+        included.transaction = SharableTransaction {
+            offset: tx.offset,
+            length: tx.length,
+        };
+        included.compute_units = *compute_units;
+        included.fee_payer_balance = *fee_payer_balance;
+    }
+
+    fn not_included_transaction(&mut self, tx: &SharableTransaction) {
+        let mut response = self.reserve_response();
+        // SAFETY: `response` is a valid pointer to a `WorkerToPackMessage`.
+        let response = unsafe { response.as_mut() };
+
+        response.tag = worker_message_types::NOT_INCLUDED;
+        // SAFETY: `response` is a valid pointer to a `WorkerToPackMessage
+        let not_included = unsafe { &mut response.inner.not_included };
+
+        not_included.transaction = SharableTransaction {
+            offset: tx.offset,
+            length: tx.length,
+        };
+        not_included.reason = 0; // TODO: set reason
     }
 
     fn resolve_message(&mut self, message: &PackToWorkerMessage) {
@@ -235,6 +351,33 @@ impl WorkerForExternal {
             let ptr = self.allocator.ptr_from_offset(sharable_transaction.offset);
             TxPtr::new(ptr, sharable_transaction.length as usize)
         }
+    }
+
+    fn tx_ptr_to_resolved_transaction_view(
+        tx_ptr: TxPtr,
+        bank: &Bank,
+    ) -> Option<RuntimeTransactionView> {
+        let view = SanitizedTransactionView::try_new_sanitized(tx_ptr).ok()?;
+        let view = RuntimeTransaction::<SanitizedTransactionView<_>>::try_from(
+            view,
+            MessageHash::Compute,
+            None,
+        )
+        .ok()?;
+
+        // Load addresses for transaction.
+        let load_addresses_result = match view.version() {
+            TransactionVersion::Legacy => Ok((None, u64::MAX)),
+            TransactionVersion::V0 => bank
+                .load_addresses_from_ref(view.address_table_lookup_iter())
+                .map(|(loaded_addresses, deactivation_slot)| {
+                    (Some(loaded_addresses), deactivation_slot)
+                }),
+        };
+        let (loaded_addresses, _deactivation_slot) = load_addresses_result.ok()?;
+
+        RuntimeTransactionView::try_from(view, loaded_addresses, bank.get_reserved_account_keys())
+            .ok()
     }
 
     // TODO: handle the case where we cannot reserve a response.
