89b566a1b8f714ac925bd1a48e7bbbfe9f3884a2
Andrew Fitzgerald <apfitzge@gmail.com>
2025-08-29 08:29:01 -0500

    shorter sleep

--- poh/src/poh_service.rs
+++ poh/src/poh_service.rs
@@ -402,7 +402,7 @@ impl PohService {
         poh_recorder: &RwLock<PohRecorder>,
         mut service_message: PohServiceMessageGuard,
     ) {
-        std::thread::sleep(Duration::from_millis(1000));
+        std::thread::sleep(Duration::from_millis(50));
         {
             let mut recorder = poh_recorder.write().unwrap();
             match service_message.take() {
