0e21bdf1ad8a5048c047a16c082fd2834aab09f8
Andrew Fitzgerald <apfitzge@gmail.com>
2026-05-19 22:15:02 +0000

    ridiculous threshold for sleep

--- core/src/banking_stage/consume_worker.rs
+++ core/src/banking_stage/consume_worker.rs
@@ -3421,7 +3421,7 @@ fn active_leader_state(
 
 const STARTING_SLEEP_DURATION: Duration = Duration::from_micros(250);
 const MAX_SLEEP_DURATION: Duration = Duration::from_millis(1);
-const IDLE_SLEEP_THRESHOLD: Duration = Duration::from_millis(400);
+const IDLE_SLEEP_THRESHOLD: Duration = Duration::from_secs(2);
 
 /// Sleeps for the specified time. Returns the next sleep duration to use.
 fn backoff(idle_duration: Duration, sleep_duration: &Duration) -> Duration {
@@ -4496,7 +4496,7 @@ mod tests {
 
     #[test]
     fn test_backoff() {
-        assert_eq!(IDLE_SLEEP_THRESHOLD, Duration::from_millis(400));
+        assert_eq!(IDLE_SLEEP_THRESHOLD, Duration::from_secs(2));
 
         let sleep_duration = STARTING_SLEEP_DURATION;
 
